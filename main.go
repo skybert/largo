bbcb346f30c05bf7e9e6402691316af81aff08d7
Torstein Krause Johansen <torstein@skybert.net>
2025-10-15 13:44:58 +0200

    Support posting body

--- main.go
+++ main.go
@@ -2,40 +2,47 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
 	"net/http"
 	"strconv"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humago"
-
 	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
 )
 
 type TokenResponse struct {
-	TraceId string `header:x-trace-id`
+	TraceId string `header: x-trace-id`
 	Body    struct {
-		AccessToken string `json:"access_token" example:"ey.." doc:"The keys to the mines of Moria"`
+		AccessToken string `json:"access_token" example:"ey.." doc:"The keys to the mines of Moria" validate:"required"`
 	}
 }
 
-func handleToken(ctx context.Context, input *struct {
+type TokenRequest struct {
 	Name string `header:"x-name" required:false`
-}) (*TokenResponse, error) {
+	Body struct {
+		Age int `json:"age"`
+	}
+}
+
+func handleToken(ctx context.Context, input *TokenRequest) (*TokenResponse, error) {
 	resp := &TokenResponse{}
 	resp.TraceId = "largo-" + strconv.Itoa(rand.Int())
-	resp.Body.AccessToken = "your access token, Sir"
+	resp.Body.AccessToken = "hi " + input.Name + ", you may be " + fmt.Sprint(input.Body.Age) + " old"
 	return resp, nil
 }
 
 func main() {
 	router := http.NewServeMux()
-	api := humago.New(router, huma.DefaultConfig("My API", "1.0.0"))
-	// TODO: Register operations...
+	humaConf := huma.DefaultConfig("Largo", "42.0.0")
+	humaConf.DocsPath = "/swagger-ui"
 
-	// Register GET /greeting/{name} handler.
+	api := humago.New(router, humaConf)
 	huma.Post(api, "/token", handleToken)
 
-	// Start the server!
-	http.ListenAndServe("127.0.0.1:8888", router)
+	port := "8888"
+	fmt.Printf("Starting Largo on port %v\n", port)
+
+	http.ListenAndServe("127.0.0.1:"+port, router)
 }
