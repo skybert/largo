e07f3a7c0eea24e2c3bbe7fb429822ef85e8e80b
Torstein Krause Johansen <torstein@skybert.net>
2025-10-15 14:08:26 +0200

    Write OpenAPI spec to file

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require github.com/danielgtaylor/huma/v2 v2.34.1
 require (
 	github.com/fxamacker/cbor/v2 v2.8.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
+	go.yaml.in/yaml/v3 v3.0.4 // indirect
 )
--- main.go
+++ main.go
@@ -5,31 +5,41 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humago"
 	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
+	"go.yaml.in/yaml/v3"
 )
 
 type TokenResponse struct {
 	TraceId string `header: x-trace-id`
 	Body    struct {
-		AccessToken string `json:"access_token" example:"ey.." doc:"The keys to the mines of Moria" validate:"required"`
+		AccessToken string `json:"access_token" example:"ey.." doc:"The keys to the mines of Moria"`
 	}
 }
 
 type TokenRequest struct {
-	Name string `header:"x-name" required:false`
+	Name string `header:"x-name"`
 	Body struct {
-		Age int `json:"age"`
+		Name string `json:"name" required:"false"`
+		Age  int    `json:"age" minimum:"18"`
+		URN  string `json:"urn" pattern:"^skybert.net:foo:"`
 	}
 }
 
 func handleToken(ctx context.Context, input *TokenRequest) (*TokenResponse, error) {
 	resp := &TokenResponse{}
 	resp.TraceId = "largo-" + strconv.Itoa(rand.Int())
-	resp.Body.AccessToken = "hi " + input.Name + ", you may be " + fmt.Sprint(input.Body.Age) + " old"
+	name := input.Name
+	if name == "" {
+		name = input.Body.Name
+	}
+	resp.Body.AccessToken = "hi " + name +
+		", you may be " + fmt.Sprint(input.Body.Age) + " old" +
+		" urn: " + input.Body.URN
 	return resp, nil
 }
 
@@ -41,8 +51,20 @@ func main() {
 	api := humago.New(router, humaConf)
 	huma.Post(api, "/token", handleToken)
 
+	writeOpenAPISpecToFile(api)
+
 	port := "8888"
 	fmt.Printf("Starting Largo on port %v\n", port)
 
 	http.ListenAndServe("127.0.0.1:"+port, router)
+}
+
+func writeOpenAPISpecToFile(api huma.API) {
+	openAPISpec := api.OpenAPI()
+	yamlBytes, err := yaml.Marshal(openAPISpec)
+	if err != nil {
+		panic(err)
+	}
+
+	os.WriteFile("openapi.yaml", yamlBytes, 0644)
 }
